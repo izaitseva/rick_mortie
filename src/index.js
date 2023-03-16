f3c0487d73456b87c10e73518081daf2e3521f99
Iryna Zaitseva <zaitseva02@gmail.com>
2023-03-16 17:43:04 +0200

    basename

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/rick_morty">
+    <BrowserRouter basename="https://izaitseva.github.io/rick_morty/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
